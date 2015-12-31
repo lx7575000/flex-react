e974adfc90aa0ea5cbcb6613ea29cd212f63fa68
lx7575000 <liu_narcisus@163.com>
2015-12-31 14:23:55 +0800

    add connect Component and ProductStore Store

---
+++ js/components/FilterBtn.js
@@ -0,0 +1,27 @@
+const React = require('react');
+const ProductStore = require('../stores/ProductStore');
+const {toggleShowOnlyLike} = ProductStore;
+const connect = require('./connect');
+
+class FilterBtn extends React.Component{
+    handleFilterBtnClick() {
+        toggleShowOnlyLike();
+    }
+
+    render() {
+        return (
+            <img 
+                src={this.props.isFiltered ? "img/heart-liked.svg" : "img/heart.svg"} 
+                onClick={this.handleFilterBtnClick.bind(this)}
+                className="filter__heart" 
+            />
+        );
+    }
+}
+
+module.exports = FilterBtn;
+
+@connect(ProductStore, 'isFiltered')
+class ConnectedFilterBtn extends FilterBtn {}
+
+module.exports = ConnectedFilterBtn;
---
+++ js/stores/ProductStore.js
@@ -0,0 +1,122 @@
+const LikeStore = require('./LikeStore');
+const {
+    likedItems
+} = LikeStore;
+const EventEmitter = require('events');
+
+let emitter = new EventEmitter();
+
+function emitChange() {
+    emitter.emit('change');
+}
+
+let _products = {
+    "jameson-vulc": {
+        id: "jameson-vulc",
+        name: "Jameson Vulc",
+        price: 64.99,
+        imagePath: "img/shoes/jameson-vulc-brown-gum-orig.png",
+        gender: "man",
+    },
+
+    "marana-x-hook-ups": {
+        id: "marana-x-hook-ups",
+        name: "Marana X Hook-Up",
+        price: 79.99,
+        imagePath: "img/shoes/marana-x-hook-ups-black-orig.png",
+        gender: "man",
+    },
+
+    "jameson-e-lite": {
+        id: "jameson-e-lite",
+        name: "Jameson E-Lite",
+        price: 69.99,
+        imagePath: "img/shoes/jameson-e-lite-maroon-orig.png",
+        gender: "man",
+    },
+
+    "jameson-e-lite-julian-davidson-4": {
+        id: "jameson-e-lite-julian-davidson-4",
+        name: "Jameson E-Lite Julian Davidson",
+        price: 74.99,
+        imagePath: "img/shoes/jameson-e-lite-julian-davidson-4-black-gum-orig.png",
+        gender: "man",
+    },
+
+    "scout-womens-6": {
+        id: "scout-womens-6",
+        name: "Scout Women's",
+        imagePath: "img/shoes/scout-womens-6-teal-orig.png",
+        price: 59.99,
+        gender: "woman",
+    },
+
+    "scout-womens-coco-ho-5": {
+        id: "scout-womens-coco-ho-5",
+        name: "Scout Women's Coco Ho",
+        imagePath: "img/shoes/scout-womens-coco-ho-5-olive-white-orig.png",
+        price: 59.99,
+        gender: "woman",
+    },
+
+    "jameson-2-womens-8": {
+        id: "jameson-2-womens-8",
+        name: "Jameson 2 Women's",
+        imagePath: "img/shoes/jameson-2-womens-8-black-white-gum-orig.png",
+        price: 59.99,
+        gender: "woman",
+    },
+
+    "corby-womens-2": {
+        id: "corby-womens-2",
+        name: "Corby Women's",
+        imagePath: "img/shoes/corby-womens-2-tan-white-orig.png",
+        price: 44.99,
+        gender: "woman",
+    }
+};
+
+let _showOnlyLike = false;
+
+module.exports = {
+    products() {
+        return _products;
+    },
+
+        filteredProducts() {
+            if (!_showOnlyLike) {
+                //如果没要求只显示喜欢的一类产品，则全部显示。
+                return this.products();
+            } else {
+                //否则只显示喜欢的商品
+                let liked = likedItems();
+                let filteredItems = {};
+                for (let id in _products) {
+                    let item = _products[id];
+                    if (id in liked) {
+                        filteredItems[id] = item;
+                    }
+                }
+                return filteredItems;
+            }
+        },
+        toggleShowOnlyLike(flag) {
+            if (typeof flag === 'boolean') {
+                _showOnlyLike = flag;
+            } else {
+                _showOnlyLike = !_showOnlyLike;
+            }
+
+            emitChange();
+        },
+        isFiltered() {
+            return _showOnlyLike;
+        },
+        addChangeListener(callback) {
+            emitter.addListener("change", callback)
+        },
+
+        removeChangeListener(callback) {
+            emitter.removeListener("change", callback)
+        },
+}
